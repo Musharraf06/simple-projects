366a81594f7985534b7a3fa04a5edd97af045846
Musharraf06 <41165666+Musharraf06@users.noreply.github.com>
2022-10-17 15:15:17 +0530

    feat: loading content from markdown file

--- markdown-previewer/script.js
+++ markdown-previewer/script.js
@@ -1,8 +1,23 @@
 const markdown_container = document.querySelector('.markdown');
 const preview_container = document.querySelector('.preview');
+const file_selecter = document.querySelector('.md_file_selecter');
 
-function update_previewer() {
-  preview_container.innerHTML = marked.parse(
-    markdown_container.value
-  );
+function load_md_file() {
+  if (file_selecter && file_selecter.files[0]) {
+    const myFile = file_selecter.files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener('load', function (e) {
+      markdown_container.value = e.target.result;
+      update_previewer(e.target.result);
+    });
+
+    reader.readAsBinaryString(myFile);
+  }
 }
+
+function update_previewer(value) {
+  preview_container.innerHTML = marked.parse(value);
+}
+
+update_previewer(markdown_container.value);
